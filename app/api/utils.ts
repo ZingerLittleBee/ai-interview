5d1a3fa4606efa7ec9add4b3b254d8d8b31aae3b
ZingerLittleBee <6970999@gmail.com>
2024-08-17 01:24:00 +0800

    pdf to string

---
+++ app/api/utils.ts
@@ -0,0 +1,26 @@
+import PDFParser from "pdf2json";
+
+export const pdfToString = (pdfPath: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const pdfParser = new PDFParser(null, true);
+
+        pdfParser.on("pdfParser_dataError", (errData) => {
+            reject(errData.parserError);
+        });
+
+        pdfParser.on("pdfParser_dataReady", () => {
+            resolve(pdfParser.getRawTextContent());
+        });
+
+        pdfParser.loadPDF(pdfPath);
+    });
+};
+
+const pdfPath = "resume.pdf";
+pdfToString(pdfPath)
+    .then((text) => {
+        console.log(text);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
