6090ca08719bb9c1ff300c6b50ee451e767ac171
fuxuewei <fuxuewei@tezign.com>
2024-08-17 00:44:29 +0800

    init: ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,37 @@
 "use client";
 
 import { useState } from 'react';
+import InterviewLayout from '@/components/InterviewLayout';
 
 export default function Home() {
     const [generation, setGeneration] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
-        return (
-            <div>
-                <div
-                    onClick={async () => {
-                        setIsLoading(true);
+    // return (
+    //     <div>
 
-                        await fetch('/api/completion', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                prompt: 'Why is the sky blue?',
-                            }),
-                        }).then(response => {
-                            response.json().then(json => {
-                                setGeneration(json.text);
-                                setIsLoading(false);
-                            });
-                        });
-                    }}
-                >
-                    Generate
-                </div>
+    //         <div
+    //             onClick={async () => {
+    //                 setIsLoading(true);
 
-                {isLoading ? 'Loading...' : generation}
-            </div>
-        );
+    //                 await fetch('/api/completion', {
+    //                     method: 'POST',
+    //                     body: JSON.stringify({
+    //                         prompt: 'Why is the sky blue?',
+    //                     }),
+    //                 }).then(response => {
+    //                     response.json().then(json => {
+    //                         setGeneration(json.text);
+    //                         setIsLoading(false);
+    //                     });
+    //                 });
+    //             }}
+    //         >
+    //             Generate
+    //         </div>
+
+    //         {isLoading ? 'Loading...' : generation}
+    //     </div>
+    // );
+    return <InterviewLayout />
 }
