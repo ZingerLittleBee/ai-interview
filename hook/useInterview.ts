5cf7921bf9a52b3f607b862f28de662912e8a44a
ranbo <71162666+RustLoverTheCoder@users.noreply.github.com>
2024-08-18 10:03:18 +0800

    feat: add textToSpeech to chat

--- hook/useInterview.ts
+++ hook/useInterview.ts
@@ -1,13 +1,17 @@
-import {useChat} from "ai/react";
+import { textToSpeech } from "@/lib/tts";
+import { useChat } from "ai/react";
 
 export default function useInterview() {
-    const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    onFinish: (message) => {
+      textToSpeech(message.content);
+    },
+  });
 
-    return {
-        messages,
-        input,
-        handleInputChange,
-        handleSubmit
-    }
-
+  return {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+  };
 }
